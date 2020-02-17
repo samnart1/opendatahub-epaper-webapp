dd672c0a3c3fc18487bd4023220a3e2149b77023
Peter Moser <p.moser@noi.bz.it>
2020-02-17 14:00:03 +0100

    Build: Add Jenkinsfiles

---
+++ Jenkinsfile-Test.groovy
@@ -0,0 +1,33 @@
+pipeline {
+    agent {
+        dockerfile {
+            filename 'docker/dockerfile-node'
+            additionalBuildArgs '--build-arg JENKINS_USER_ID=`id -u jenkins` --build-arg JENKINS_GROUP_ID=`id -g jenkins`'
+        }
+    }
+
+    environment {
+        AWS_ACCESS_KEY_ID = credentials('AWS_ACCESS_KEY_ID')
+        AWS_SECRET_ACCESS_KEY = credentials('AWS_SECRET_ACCESS_KEY')
+
+        VUE_APP_API_URL = "https://api.epaper.testingmachine.eu"
+    }
+
+    stages {
+        stage('Dependencies') {
+            steps {
+                sh 'yarn install'
+            }
+        }
+        stage('Build') {
+            steps {
+                sh 'yarn run build'
+            }
+        }
+        stage('Upload') {
+            steps {
+                s3Upload(bucket: 'epaper-test', acl: 'PublicRead', file: './dist')
+            }
+        }
+    }
+}
