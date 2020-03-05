6b837669d385a2928e2d0df0b54f6682b7718fd9
dulvui <simon.dalvai@gmail.com.com>
2020-03-05 01:37:20 +0100

    removed vuetify. Using only boostrap now

--- src/main.js
+++ src/main.js
@@ -1,20 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-import 'vuetify/dist/vuetify.min.css' 
-import vuetify from './plugins/vuetify';
 import store from './store/store'
 import router from './router'
 
 
 Vue.use(require('vue-moment'));
-Vue.use(vuetify)
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
-  vuetify,
   created () {
     this.$store.dispatch('loadData')
   },
