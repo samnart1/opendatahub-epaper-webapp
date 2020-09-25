66f58b1052aa6d369bb6dd25b132d93d7af52511
Peter Moser <p.moser@noi.bz.it>
2020-09-25 16:49:56 +0200

    Use correct API URL

--- Jenkinsfile-Test.groovy
+++ Jenkinsfile-Test.groovy
@@ -10,7 +10,7 @@ pipeline {
         AWS_ACCESS_KEY_ID = credentials('AWS_ACCESS_KEY_ID')
         AWS_SECRET_ACCESS_KEY = credentials('AWS_SECRET_ACCESS_KEY')
 
-        VUE_APP_API_URL = "https://api.epaper.testingmachine.eu"
+        VUE_APP_API_URL = "https://api.epaper.opendatahub.testingmachine.eu"
     }
 
     stages {
